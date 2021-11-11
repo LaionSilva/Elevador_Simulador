b254b1fa8c9e68ee7457c700ff3ddd81c838c67c
LaionSilva <laion@ucl.br>
2021-11-10 23:03:44 -0300

    v0.1 tela principal e main

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,10 +21,50 @@ namespace Elevador_Simulador
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool subir = false;
+
+        private byte[] digitos;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
 
+
+        #region botões_internos
+        private void button_t_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_em_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
+
+        private void DisplayAndar(byte digito)
+        {
         }
     }
 }
